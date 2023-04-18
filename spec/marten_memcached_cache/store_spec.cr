0161ebbaaff06adb9d18e38fcfbcfb28c5c01bc3
Morgan Aubert <morgan@aubert.email>
2023-04-17 22:11:04 -0400

    Add support for increment and decrement operations

--- spec/marten_memcached_cache/store_spec.cr
+++ spec/marten_memcached_cache/store_spec.cr
@@ -38,6 +38,58 @@ describe MartenRedisCache::Store do
     end
   end
 
+  describe "#decrement" do
+    it "can decrement an existing integer value" do
+      2.times { Marten.cache.increment("foo") }
+
+      Marten.cache.decrement("foo").should eq 1
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq 1
+    end
+
+    it "can decrement an existing integer value when a namespace is used" do
+      store = MartenRedisCache::Store.new(namespace: "ns", uri: ENV_SETTINGS["REDIS_URI"].as(String))
+      2.times { store.increment("foo") }
+
+      store.decrement("foo").should eq 1
+      store.read("foo", raw: true).try(&.to_i).should eq 1
+    end
+
+    it "can decrement an existing integer value for a key that is not expired" do
+      2.times { Marten.cache.increment("foo", expires_in: 2.hours) }
+
+      Marten.cache.decrement("foo").should eq 1
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq 1
+    end
+
+    it "can decrement an existing integer value by a specific amount" do
+      5.times { Marten.cache.increment("foo") }
+
+      Marten.cache.decrement("foo", amount: 3).should eq 2
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq 2
+    end
+
+    it "decrements as expected in case the key does not exist" do
+      Marten.cache.decrement("foo").should eq -1
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq -1
+
+      Marten.cache.decrement("bar", amount: 2).should eq -2
+      Marten.cache.read("bar", raw: true).try(&.to_i).should eq -2
+    end
+
+    it "writes the amount value to the cache in case the key is expired" do
+      5.times { Marten.cache.increment("foo", expires_in: 1.second) }
+      5.times { Marten.cache.increment("bar", expires_in: 1.second) }
+
+      sleep 2
+
+      Marten.cache.decrement("foo").should eq -1
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq -1
+
+      Marten.cache.decrement("bar", amount: 2).should eq -2
+      Marten.cache.read("bar", raw: true).try(&.to_i).should eq -2
+    end
+  end
+
   describe "#delete" do
     it "deletes the entry associated with the passed key and returns true" do
       Marten.cache.write("foo", "bar")
@@ -60,6 +112,58 @@ describe MartenRedisCache::Store do
 
     it "returns false if the passed key is not in the cache" do
       Marten.cache.exists?("foo").should be_false
+    end
+  end
+
+  describe "#increment" do
+    it "can increment an existing integer value" do
+      2.times { Marten.cache.increment("foo") }
+
+      Marten.cache.increment("foo").should eq 3
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq 3
+    end
+
+    it "can increment an existing integer value when a namespace is used" do
+      store = MartenRedisCache::Store.new(namespace: "ns", uri: ENV_SETTINGS["REDIS_URI"].as(String))
+      2.times { store.increment("foo") }
+
+      store.increment("foo").should eq 3
+      store.read("foo", raw: true).try(&.to_i).should eq 3
+    end
+
+    it "can increment an existing integer value for a key that is not expired" do
+      2.times { Marten.cache.increment("foo", expires_in: 2.hours) }
+
+      Marten.cache.increment("foo").should eq 3
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq 3
+    end
+
+    it "can increment an existing integer value by a specific amount" do
+      5.times { Marten.cache.increment("foo") }
+
+      Marten.cache.increment("foo", amount: 3).should eq 8
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq 8
+    end
+
+    it "increments as expected in case the key does not exist" do
+      Marten.cache.increment("foo").should eq 1
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq 1
+
+      Marten.cache.increment("bar", amount: 2).should eq 2
+      Marten.cache.read("bar", raw: true).try(&.to_i).should eq 2
+    end
+
+    it "writes the amount value to the cache in case the key is expired" do
+      5.times { Marten.cache.increment("foo", expires_in: 1.second) }
+      5.times { Marten.cache.increment("bar", expires_in: 1.second) }
+
+      sleep 2
+
+      Marten.cache.increment("foo").should eq 1
+      Marten.cache.read("foo", raw: true).try(&.to_i).should eq 1
+
+      Marten.cache.increment("bar", amount: 2).should eq 2
+      Marten.cache.read("bar", raw: true).try(&.to_i).should eq 2
     end
   end
 
--- src/marten_redis_cache/store.cr
+++ src/marten_redis_cache/store.cr
@@ -27,6 +27,65 @@ module MartenRedisCache
       namespace ? delete_namespaced_keys : client.flushdb
     end
 
+    def decrement(
+      key : String,
+      amount : Int32 = 1,
+      expires_at : Time? = nil,
+      expires_in : Time::Span? = nil,
+      version : Int32? = nil,
+      race_condition_ttl : Time::Span? = nil,
+      compress : Bool? = nil,
+      compress_threshold : Int32? = nil
+    ) : Int
+      normalized_key = normalize_key(key.to_s)
+
+      effective_expires_in = if !expires_at.nil?
+                               expires_at.to_utc - Time.utc
+                             else
+                               expires_in.nil? ? self.expires_in : expires_in
+                             end
+
+      effective_expires_in = adapt_expiry_for_race_condition(effective_expires_in, race_condition_ttl)
+
+      client.decrby(normalized_key, amount).tap do
+        client.expire(normalized_key, effective_expires_in.not_nil!.total_seconds.to_i) if effective_expires_in
+      end
+    end
+
+    def increment(
+      key : String,
+      amount : Int32 = 1,
+      expires_at : Time? = nil,
+      expires_in : Time::Span? = nil,
+      version : Int32? = nil,
+      race_condition_ttl : Time::Span? = nil,
+      compress : Bool? = nil,
+      compress_threshold : Int32? = nil
+    ) : Int
+      normalized_key = normalize_key(key.to_s)
+
+      effective_expires_in = if !expires_at.nil?
+                               expires_at.to_utc - Time.utc
+                             else
+                               expires_in.nil? ? self.expires_in : expires_in
+                             end
+
+      effective_expires_in = adapt_expiry_for_race_condition(effective_expires_in, race_condition_ttl)
+
+      client.incrby(normalized_key, amount).tap do
+        client.expire(normalized_key, effective_expires_in.not_nil!.total_seconds.to_i) if effective_expires_in
+      end
+    end
+
+    private def adapt_expiry_for_race_condition(expires_in : Time::Span? = nil, race_condition_ttl : Time::Span? = nil)
+      # Add an extra 5 minutes to the expiry of the memcached entry to allow for race condition TTL reads.
+      if !expires_in.nil? && !race_condition_ttl.nil?
+        expires_in += 5.minutes
+      end
+
+      expires_in
+    end
+
     private def delete_entry(key : String) : Bool
       client.del(key) == 1_i64
     end
@@ -44,26 +103,22 @@ module MartenRedisCache
       end
     end
 
-    private def read_entry(key : String) : Marten::Cache::Entry?
-      deserialize_entry(client.get(key))
+    private def read_entry(key : String) : String?
+      client.get(key)
     end
 
     private def write_entry(
       key : String,
-      entry : Marten::Cache::Entry,
+      value : String,
       expires_in : Time::Span? = nil,
-      race_condition_ttl : Time::Span? = nil,
-      compress : Bool? = nil,
-      compress_threshold : Int32? = nil
+      race_condition_ttl : Time::Span? = nil
     )
-      serialized_entry = serialize_entry(entry, compress, compress_threshold)
-
       # Add an extra 5 minutes to the expiry of the Redis entry to allow for race condition TTL reads.
       if !expires_in.nil? && !race_condition_ttl.nil?
         expires_in += 5.minutes
       end
 
-      client.set(key, serialized_entry, expires_in.try(&.total_seconds.to_i))
+      client.set(key, value, expires_in.try(&.total_seconds.to_i))
       true
     end
   end
